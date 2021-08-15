f635ecccf4ab0de974710ba6b77b85edc83198ac
Itzel Contreras <itselfcg@gmail.com>
2021-08-15 13:40:26 -0700

    Scroll down style was changed. New scroll up button.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import * as AOS from 'aos';
+declare var $: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
   title = 'baja-discover-v2';
   ngOnInit(): void {
     AOS.init();
 
+    $(document).scroll(function () {
+      var x = $(document).scrollTop();
+      if (x > 500) {
+        $('.scrollup').stop().fadeIn(100);
+        $('.scrolldown').stop().fadeOut(100);
+      } else {
+        $('.scrolldown').stop().fadeIn(100);
+        $('.scrollup').stop().fadeOut(100);
+      }
+    });
   }
-
 }
