7635d71b3e08d7904bad8e08460b412f3c9c7823
Itzel Contreras <itselfcg@gmail.com>
2021-08-16 12:06:27 -0700

    Carousel size changes when page is resized

--- src/app/tours/tours.component.ts
+++ src/app/tours/tours.component.ts
@@ -32,24 +32,24 @@ export class ToursComponent implements OnInit {
   tours: Tour[] = tours;
   toursSelected: Tour = tours[0];
   curPage: number = 1;
-  pageSize: number = 3;
+  pageSize: number = 0;
   animationStyle = 0;
   constructor() {}
 
   ngOnInit(): void {
-  /*   if (window.innerWidth > 768) {
+  if (window.innerWidth > 768) {
       this.pageSize = 3;
     } else {
       this.pageSize = 2;
-    } */
+    }
   }
   @HostListener('window:resize', ['$event'])
   onResize(event: Event) {
-   /*  if (window.innerWidth > 768) {
+    if (window.innerWidth > 768) {
       this.pageSize = 3;
     } else {
       this.pageSize = 2;
-    } */
+    }
   }
 
   selectTour(tour: Tour) {
