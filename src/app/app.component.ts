f52fba08172def19df4cc778ce044885675d2b99
Itzel Contreras <itselfcg@gmail.com>
2021-08-17 12:46:07 -0700

    Fix for aos library not loading elements bug

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HostListener } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
 import * as AOS from 'aos';
 declare var $: any;
@@ -10,18 +11,23 @@ declare var $: any;
 export class AppComponent implements OnInit {
   title = 'baja-discover-v2';
   ngOnInit(): void {
-    AOS.init({once: true});
+    AOS.init({ once: true });
     $('.scrollup').stop().fadeOut(100);
+  }
 
-    $(document).scroll(function () {
-      var x = $(document).scrollTop();
-      if (x > 500) {
-        $('.scrollup').stop().fadeIn(100);
-        $('.scrolldown').stop().fadeOut(100);
-      } else {
-        $('.scrolldown').stop().fadeIn(100);
-        $('.scrollup').stop().fadeOut(100);
-      }
-    });
+  ngAfterViewInit() {
+    window.addEventListener('load', AOS.refresh);
+  }
+
+  @HostListener('window:scroll', ['$event'])
+  onScroll(event: Event) {
+    var x = $(document).scrollTop();
+    if (x > 500) {
+      $('.scrollup').stop().fadeIn(100);
+      $('.scrolldown').stop().fadeOut(100);
+    } else {
+      $('.scrolldown').stop().fadeIn(100);
+      $('.scrollup').stop().fadeOut(100);
+    }
   }
 }
